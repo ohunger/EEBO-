47d0a1931bf8f9726c95e7b173e67446d495364b
Betram Lalusha <lalushabetm@gmail.com>
2022-11-10 22:57:30 -0800

    adding firebase

---
+++ eebo/src/firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAljI76OPn24ZPicYz27Yu5DkptneiWGlY",
+  authDomain: "eebo-c2acf.firebaseapp.com",
+  projectId: "eebo-c2acf",
+  storageBucket: "eebo-c2acf.appspot.com",
+  messagingSenderId: "385439455736",
+  appId: "1:385439455736:web:d4c6229fdd6524de769e0d"
+};
