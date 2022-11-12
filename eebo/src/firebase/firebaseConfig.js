903444b227e8d1cef3656149b6eba0fe106e0121
Betram Lalusha <lalushabetm@gmail.com>
2022-11-12 11:15:31 -0800

    adding imports

--- eebo/src/firebase/firebaseConfig.js
+++ eebo/src/firebase/firebaseConfig.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+const { getAuth } = require('firebase/auth');
+const { getFirestore} = require('firebase/firestore');
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
